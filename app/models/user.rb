9aea9b0e57d92ec89d6987d94eeaca06b34ac84d
Nick Goddard <nicko777@gmail.com>
2010-11-25 22:04:16 +0000

    User signup complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,14 +16,14 @@ class User < ActiveRecord::Base
 	attr_accessor		:password
 	attr_accessible 	:name, :email, :password, :password_confirmation
 	
-	validates 			:name, 		:presence 	=> true
-									:length		=> { :maximum -> 50 }
-	validates			:email,		:presence 	=> true
-									:format		=> { :with => email_regex }
-									:uniqueness	=> { :case_sensitive => false }
-	validates			:password	:presence	=>	true,
-									:confirmation	=> true,
-									:length		=> { :within => 6..40 }
+	validates 			:name, 		:presence => true,
+									:length => { :maximum => 50 }
+	validates			:email,		:presence => true,
+									:format => { :with => email_regex },
+									:uniqueness => { :case_sensitive => false }
+	validates			:password,	:presence =>	true,
+									:confirmation => true,
+									:length => { :within => 6..40 }
 	before_save :encrypt_password
 	
 	def has_password?(submitted_password)
